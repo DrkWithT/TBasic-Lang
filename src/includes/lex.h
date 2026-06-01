4788a2e22bb79a62769a3e668181eb199943d862
drkwitht <derektan3510@gmail.com>
2026-06-01 14:29:49 -0700

    Allow no-ELSE IF statements (if block required)

--- src/includes/lex.h
+++ src/includes/lex.h
@@ -17,7 +17,6 @@ typedef enum token_tag_t {
     tk_keyword_ret,
     tk_keyword_fun,
     tk_keyword_end,
-    tk_keyword_self,
     tk_identifier,
     tk_none,
     tk_true,
@@ -37,7 +36,6 @@ typedef enum token_tag_t {
     tk_os_and,
     tk_os_or,
     tk_os_bind_equals,  // ? `:=` is for mutating a variable
-    tk_os_ref_method_of,    // ? `->` for accessing a named member
     tk_comma,
     tk_colon,
     tk_semicolon,
