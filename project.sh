abb70f92b8f7ae60f29bca8bef4f745e1e1dc6c7
drkwitht <derektan3510@gmail.com>
2026-08-13 14:08:44 -0700

    Attempting project shell script fix for bash.

--- project.sh
+++ project.sh
@@ -1,25 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
 mode="$1";
 
-if [[ $mode = "cl" ]]; then
+if [ $mode = "cl" ]
+then
     rm -rf ./build/ || printf "Cleaned build files";
-elif [[ $mode = "bd" ]]; then
+elif [ $mode = "bd" ]
+then
     cmake -S . -B build -DCMAKE_BUILD_TYPE:STRING=Debug && cmake --build build;
-    mv ./build/compile_commands.json .;
-elif [[ $mode = "br" ]]; then
+elif [ $mode = "br" ]
+then
     cmake -S . -B build -DCMAKE_BUILD_TYPE:STRING=Release && cmake --build build;
-    mv ./build/compile_commands.json .;
     strip ./build/tbasic -o ./tbasic;
-elif [[ $mode = "bp" ]]; then
+elif [ $mode = "bp" ]
+then
     cmake -S . -B build -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo && cmake --build build;
-    mv ./build/compile_commands.json .;
-elif [[ $mode = "sd" ]]; then
+elif [ $mode = "sd" ]
+then
     cmake -S . -B build -DCMAKE_BUILD_TYPE:STRING=Debug -DCREATE_TBASIC_SHARED:BOOL=1 && cmake --build build;
-    mv ./build/compile_commands.json .;
-elif [[ $mode = "sr" ]]; then
+elif [ $mode = "sr" ]
+then
     cmake -S . -B build -DCMAKE_BUILD_TYPE:STRING=Release -DCREATE_TBASIC_SHARED:BOOL=1 && cmake --build build;
-    mv ./build/compile_commands.json .;
 else
     printf "Usage:\n\tcl: clean build files\n\tbd: build debug\n\tbr: build release\n\tbp: build profile\n\tsd: build shared lib & demo (debug)\n\tsr: build shared lib & demo (release)\n";
 fi
