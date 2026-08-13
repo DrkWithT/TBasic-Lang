9fa320e35c2527a97891891977cebbbef13ba0f6
drkwitht <derektan3510@gmail.com>
2026-08-13 13:59:23 -0700

    Update TAILCALL macro to support GCC musttail.

--- src/vm.c
+++ src/vm.c
@@ -6,6 +6,15 @@
 #include "obj_dict.h"
 #include "obj_exception.h"
 
+#ifdef __clang__
+    #define TAILCALL __attribute((musttail))
+#elif defined(__GNUC__)
+    #define TAILCALL __attribute__((musttail))
+#else
+    #error "TBasic requires TCO in its VM."
+#endif
+
+
 
 static OpFunc opcode_handlers[] = {
     fn_nop,
